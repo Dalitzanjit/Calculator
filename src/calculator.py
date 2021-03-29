61822c8fd0edf584992ff067852955393e74a1aa
Dalitzanjit <ds737@njit.edu>
2021-03-28 22:39:43 -0400

    Feature: setup calculator with result property

---
+++ src/calculator.py
@@ -0,0 +1,13 @@
+class Calculator:
+    result = 0
+
+    def _init_(self):
+        pass
+
+    def add(self, a, b):
+        self.result = addition(a, b)
+        return self.result
+
+    def subtract(self, a, b):
+        self.result = subtraction(a, b)
+        return self.result
