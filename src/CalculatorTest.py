c5e37f6be525fce94f61cfc1676eeea4e5b39ec8
Dalitzanjit <ds737@njit.edu>
2021-04-05 11:25:54 -0400

    Task: ran 1 test , ok.

--- src/CalculatorTest.py
+++ src/CalculatorTest.py
@@ -4,20 +4,14 @@ from CsvReader import CsvReader
 
 
 class MyTestCase(unittest.TestCase):
-    def setUp(self) -> None:
-        self.calculator = Calculator()
 
-    def test_instantiate_calculator(self):
-        self.assertIsInstance(self.calculator, Calculator)
+    def test_instantiate_Calculator(self):
+        calculator = Calculator()
+        self.assertIsInstance(calculator, Calculator)
 
-    def test_subtraction(self):
-        test_data = CsvReader('/src/subtraction.csv').data
-        for row in test_data:
-            self.assertEqual(self.calculator.subtract(row['Value 1'], row['Value 2']), int(row['Result']))
-            self.assertEqual(self.calculator.result, int(row['Result']))
-
-    def test_results_property(self):
-        self.assertEqual(self.calculator.result, 0)
+    def test_results_property_calculator(self):
+        calculator = Calculator()
+        self.assertEqual(calculator.result, 4)
 
 
 if __name__ == '__main__':
--- src/calculator.py
+++ src/calculator.py
@@ -1,31 +1,11 @@
 from CsvReader import CsvReader
 
-def addition(a, b):
-    c = a + b
-    return c
-
-def subtraction(a, b):
-    c = a - b
-    a = int(a)
-    b = int(b)
-    c = b - a
-    return c
-
-def mean(data):
- mean = data
- return mean
 
 class Calculator:
-    result = 0
-
-    def subtract(self, a, b):
-        self.result = subtraction (a, b)
-        return self.result
-
-class CSVStats(Calculator):
-    data = []
+    result = 4
 
 
-def _init_(self, data_file):
-    self.data = CsvReader(data_file)
+def _init_(self):
+    x = 2 + 2
+    self.result = x;
     pass
